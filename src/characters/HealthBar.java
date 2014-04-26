d266be3504e1fef96f5f70a8d3a3dcb8a2b43dab
Rafael Abbondanza <abbondanza.r@gmail.com>
2014-04-26 01:33:37 -0400

    Fixed collision detection to work looking at either direction..

---
+++ src/characters/HealthBar.java
@@ -0,0 +1,112 @@
+package characters;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+//to make character have health, in main class, just make a character object and healthbar object. 
+//In main class, say character.health = healthbar.health
+
+
+
+/**
+ * Constructs a health bar rectangle. It starts green then becomes gradually more red with each hit. 
+ * 
+ * @author Dana Smith
+ */
+
+
+public class HealthBar 
+{	
+    // TODO Assign x, y coordinates for both player's health bars
+    // and width and height of the health bar
+    
+	//healthBarWGreen is the green health status.
+	//healthBarWRed is the red health status. It never changes.
+	private int healthBarWGreen;
+	private int healthBarWRed;
+	private int h;
+	
+	private int y;
+	private int x;
+	
+	int currentHealth;
+	
+	/*HIT TYPES - may need to be changed*/
+	private int headHit = -10;
+	private int bodyHit = -5;
+	private int limbHit = -3;
+	
+	private String playerName;
+	
+	//Image playerFace;
+	
+	
+	// TODO Create one constructor that takes a boolean argument. True should mean that 
+	// the health bar belongs to player one and should be on the left side of the screen.
+	// False means that it belongs to player two and should be on the right side of the
+	// screen. Initialize x and y here. 
+	public HealthBar(int x, int y,int healthBarWGreen,  int healthBarWRed, int h, String playerName)
+	{
+			this.x = x;
+			this.y = y;
+			
+			this.healthBarWRed = healthBarWRed;
+			this.healthBarWGreen = healthBarWGreen;
+			this.h = h;
+			
+			this.playerName = playerName;
+			
+			this.currentHealth = healthBarWGreen;  
+			
+		}
+		
+		// TODO Take one graphics object and update the health based off of both character's
+		// current condition.
+		public void draw(Graphics g)
+		{
+			//grey box behind player name
+			g.setColor(Color.white);
+			g.fillRoundRect(x-5, y/2, healthBarWRed + 60, h*2, 20, 20);
+			
+			
+			//Player Name
+			g.setColor(Color.BLACK);
+			g.setFont(new Font("Arial", Font.PLAIN, 16));
+			g.drawString(playerName, x, y-5);
+			
+			
+			//health bar draw green on top of red rectangle
+			g.setColor(Color.red);	
+			g.fillRoundRect(x, y, healthBarWRed, h, 20, 20);
+			g.setColor(Color.green);	
+			g.fillRoundRect(x, y, healthBarWGreen, h, 20, 20);
+			
+			if (healthBarWGreen == 0 )
+			{
+				//Player dies
+			}
+			
+		}
+		
+		public void diminishBy(int hitType)
+		{
+			currentHealth = healthBarWGreen+ hitType; 
+		
+			
+			if(currentHealth > 1)
+			{
+				healthBarWGreen += hitType;
+	
+			}
+			else if (currentHealth <= 0)
+			{
+				healthBarWGreen = 0;
+								 
+				//player dies. Load character die animation  
+				//Code must be written for this.
+			}
+			
+			
+		}
+}
---
+++ src/characters/VanillaCharacter.java
@@ -0,0 +1,217 @@
+package characters;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import factory.CharacterType;
+
+public abstract class VanillaCharacter {
+	
+	int centerX;
+	int centerY;
+
+	boolean lookingRight;
+	boolean isDead;
+	boolean perfection;
+	
+	final int FLOOR_Y = 600;
+	final int GRAVITY = 2;
+	
+	SpriteAnimated a; 
+	SpriteAnimated a_lt;
+	SpriteAnimated a_rt;
+
+	// Characters need health to know how much fight is left in them
+	protected int health;
+
+	// Characters need boolean values to know what state it is currently in
+	protected boolean isOnLeft, isBlocking, isHitting, isDucking, isJumping, isKicking, isPunching, isWalking;
+
+	// Characters need hitboxes to know when they've been struck by another
+	// player
+	protected Rectangle hitBox, strikeBox, guardBox;
+
+	// Characters need to self-identify
+	protected CharacterType character;
+
+	// Characters need to be able to find their animations
+	private String imgpath;
+	
+	// Characters should know their location on screen
+	protected int x;
+	protected int y;
+
+	final int characterHeight = 300;
+	final int characterWidth = 150;
+	
+	final int HEIGHT_Y = FLOOR_Y - characterWidth;
+	int vy;
+	final int INIT_JUMP_V = 40;
+	
+	final int LEFT_PLAYER_X  = 400;
+	final int RIGHT_PLAYER_X = 800;
+	final int MOVEMENT = 13;
+	
+	/**
+	 * Defines and instantiates all attributes characters need to have
+	 * 
+	 * @param playerOne
+	 *            if true, player faces right. if false, player faces left
+	 */
+	public VanillaCharacter(boolean playerOne, CharacterType character) {
+		if (playerOne){
+			isOnLeft = false;
+			setCenterX(LEFT_PLAYER_X);
+		} else {
+			isOnLeft = true;
+			setCenterX(RIGHT_PLAYER_X);
+		}
+		
+		isOnLeft = (playerOne) ? false : true;
+		isDucking = false;
+		isBlocking = false;
+		isHitting = false;
+		isPunching = false; 
+		isJumping = false;
+		isKicking = false;
+		perfection = true;
+		
+		this.setCenterX(centerX);
+		this.character = character;
+		x = getCenterX() - (characterWidth/2);
+		y = HEIGHT_Y; 
+		
+
+		//TODO: fix these numbers
+		
+		
+		if(lookingRight)
+		{
+			hitBox   = new Rectangle( centerX - 45, y+50, 60, 150, Color.GREEN);
+			strikeBox   = new Rectangle( 0, 0 , 0, 0, Color.RED );
+			guardBox  = new Rectangle( centerX - 45, y+50, 20, 60, Color.BLUE );
+			
+		}
+		
+		else
+		{
+			hitBox  = new Rectangle( centerX - 15, y+50, 60, 150, Color.GREEN);
+			strikeBox   = new Rectangle( 0, 0, 0, 0, Color.RED );
+			guardBox   = new Rectangle( centerX + 25, y+50, 40, 60, Color.BLUE );
+		}
+	}
+
+	public abstract void punch();
+	public abstract void kick();
+	public abstract void jump();
+	public abstract void block();
+	public abstract void die();
+	public abstract void victory();
+	public abstract void takeHit();
+	public abstract void moveForward(int dx);
+	public abstract void moveBackward(int dx);
+	public abstract void fightAgainst(VanillaCharacter other); //For future AI implementions
+
+	public void resetDefaults()
+	{
+
+		if(!isPunching && !isKicking)
+		{
+			strikeBox.setWidth(0);
+			strikeBox.setHeight(0);
+		}
+		
+		if(!isBlocking) 
+		{
+			guardBox.setWidth(0);
+			guardBox.setHeight(0);
+		}
+		
+		hitBox.setYPosition(a.y + 50);
+		
+		if(!isDucking) hitBox.setWidth(60);
+		
+		
+		isBlocking = false;
+		isDucking = false;
+		isWalking = false;
+		isPunching = false;
+		isKicking = false;
+		
+	
+		a.setWidth(characterWidth);
+		a.setHeight(characterHeight);
+		
+		if(!isJumping ) a.setYPosition(HEIGHT_Y);
+		
+		a.setPos(0);
+	}
+
+	// Draw the character on the screen
+	public abstract void draw(Graphics g);
+
+	/**
+	 * Decreases health when hit
+	 * 
+	 * @param dHealth
+	 *            amount to decrease health by
+	 */
+	public void decreaseHealthBy(int dHealth) {
+		health -= dHealth;
+	}
+
+	/**
+	 * Returns the character's current health
+	 */
+	public int getHealth() {
+		return health;
+	}
+	
+	public abstract void duck();
+
+	public boolean isLookingRight() {
+		return lookingRight;
+	}
+
+	public void setLookingRight( boolean lookingRight )
+	{
+		this.lookingRight = lookingRight;
+		
+		
+		if(lookingRight)
+		{
+			a = a_rt;
+			hitBox.setXPosition(centerX - 45 );
+		}
+		
+		else
+		{
+			a = a_lt;
+			hitBox.setXPosition(centerX - 15 );
+		}
+		
+		//invertHitBoxes(greenHitBox);
+		
+	}
+	
+	
+	public int getMovement(){
+		return MOVEMENT;
+	}
+
+	public boolean isJumping() {
+		return isJumping;
+	}
+
+	public void setJumping(boolean isJumping) {
+		this.isJumping = isJumping;
+	}
+
+	public int getCenterX() {
+		return centerX;
+	}
+
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+}
